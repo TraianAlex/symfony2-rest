167747e59bd86578ede29997bdafd2b41c374211
Traian Alexandru <victor_traian@yahoo.com>
2018-09-11 13:52:18 -0400

    until l 3 handling a form

---
+++ src/AppBundle/Controller/Api/ProgrammerController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller\Api;
+
+use AppBundle\Controller\BaseController;
+use AppBundle\Entity\Programmer;
+use AppBundle\Form\ProgrammerType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProgrammerController extends BaseController
+{
+    /**
+     * @Route("/api/programmers")
+     * @Method("POST")
+     */
+    public function newAction(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+        //$programmer = new Programmer($data['nickname'], $data['avatarNumber']);
+        $programmer = new Programmer();
+        $form = $this->createForm(new ProgrammerType(), $programmer);
+        $form->submit($data);
+        //$programmer->setTagLine($data['tagLine']);
+        $programmer->setUser($this->findUserByUsername('weaverryan'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($programmer);
+        $em->flush();
+
+        return new Response('It worked. Believe me - I\'m an API');
+    }
+}
---
+++ testing.php
@@ -0,0 +1,23 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+$client = new \GuzzleHttp\Client([
+    'base_url' => 'http://localhost:8000',
+    'defaults' => [
+        'exceptions' => false
+    ]
+]);
+
+$nickname = 'ObjectOrienter'.rand(0, 999);
+$data = array(
+    'nickname' => $nickname,
+    'avatarNumber' => 5,
+    'tagLine' => 'a test dev!'
+);
+
+$response = $client->post('/api/programmers', [
+    'body' => json_encode($data)
+]);
+echo $response;
+echo "\n\n";
